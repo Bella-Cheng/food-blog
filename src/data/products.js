76b4f7896995ee5d56d6a51e849b3818894b3b00
Bella-Cheng <dhp951320@gmail.com>
2025-06-18 01:35:40 +0800

    feat : add products

---
+++ src/data/products.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: 1,
+    name: "鳳梨酥禮盒",
+    description: "經典手工鳳梨酥，酸甜適中，10 入精緻包裝",
+    price: 450,
+    image: "img-pineapplecake.jpg"
+  },
+  {
+    id: 2,
+    name: "太陽餅原味組",
+    description: "傳統中式點心，內餡飽滿、層層酥香，8 入",
+    price: 380,
+    image: "img-suncake.jpg"
+  },
+  {
+    id: 3,
+    name: "黑糖麻糬禮袋",
+    description: "外皮 Q 彈、濃郁黑糖內餡，適合長輩與小孩",
+    price: 220,
+    image: "img-mochi.jpg"
+  },
+  {
+    id: 4,
+    name: "阿里山高山茶",
+    description: "產自阿里山的清香烏龍茶，真空包裝 100g",
+    price: 600,
+    image: "img-tea.jpg"
+  },
+  {
+    id: 5,
+    name: "牛軋糖雙拼包",
+    description: "花生與蔓越莓口味各一包，口感酥脆不黏牙",
+    price: 320,
+    image: "img-nougat.jpg"
+  },
+  {
+    id: 6,
+    name: "鹹蛋黃餅乾禮盒",
+    description: "人氣鹹香餅乾，搭配蛋黃顆粒，16 片裝",
+    price: 280,
+    image: "img-cookie.jpg"
+  }
+]
--- src/views/Featured.vue
+++ src/views/Featured.vue
@@ -1,30 +1,42 @@
-<template>
-  <body class="max-w-screen">
-    <div class="max-w-6xl mx-auto">
-      <div>
-        <div>
-          <div>
-            <img src="" alt="">
-          </div>
-          <div>
-            <h2>商品</h2>
-            <p>價格 $<span>6990</span>元</p>
-            <p>原價 $<span>16990</span>元</p>
-          </div>
-          <p>說明</p>
-          <p></p>
-          <div>
-            <button class="btn" type="button">加入購物車</button>
-            <button class="btn" type="button">立即購買</button>
-          </div>
+<script setup>
+import { products } from '../data/products'
 
+function getImageUrl(fileName) {
+  return new URL(`../assets/products/${fileName}`, import.meta.url).href
+}
+
+
+</script>
+
+
+<template>
+  <div class="max-w-screen">
+    <div class="max-w-6xl mx-auto">
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 my-12">
+        <div 
+        v-for=" item in products"
+        :key="item.id"
+        class="card bg-base-100 w-96 shadow-sm">
+          <figure 
+            class="aspect-[1/1] w-full overflow-hidden rounded"
+          >
+            <img
+              :src="getImageUrl(item.image)"
+              :alt="item.name"
+              class="w-full h-full object-cover"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">{{item.name}}</h2>
+            <p>{{ item.description}}</p>
+            <p>${{ item.price}}元</p>
+            <div class="card-actions justify-end">
+              <button class="btn bg-black text-white border-none hover:bg-amber-600">加入購物車</button>
+            </div>
+          </div>
         </div>
-        
       </div>
       
     </div>
-  </body> 
-
-
-
+  </div> 
 </template>
